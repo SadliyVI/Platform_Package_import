eca6f35374a2498a7604bc2e8d66a1eb3b6a2b6a
V.Sadliy <sadliy@mail.ru>
2025-09-24 17:02:19 +0300

    Initial commit

---
+++ node_modules/@sadliyvi/jsextra_hw_platform_task_1/src/index.js
@@ -0,0 +1,10 @@
+function info() {
+    const m = new Map();
+    m.set('@sadliyvi/jsextra_hw_platform_task_1', 'JSExtra_HW_Platform_Task_1');
+    m.set('version', '1.0.0');
+    return m;
+}
+
+module.exports = {
+    info
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const ajs = require("@sadliyvi/jsextra_hw_platform_task_1/src");
+
+console.log(ajs.info());
